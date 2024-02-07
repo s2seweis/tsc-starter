015911ed1918e2d620639737fbc227f8c3c2bbe2
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-02-07 15:01:56 +0100

    adjustments

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "52c6ed788434d6d2"; }
+/******/ 		__webpack_require__.h = function() { return "d05cfccad18b645f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
